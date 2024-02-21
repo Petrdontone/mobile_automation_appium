fa0d22b3c57c147e917988d1059b8af3de4784c1
Peter Yasnetsov <pyasnetsov@ozon.ru>
2024-02-21 21:29:46 +0300

    Создал класс для создание сессии

--- helper/get_device_android.py
+++ helper/get_device_android.py
@@ -8,11 +8,10 @@ def get_connected_device_capabilities():
     for device in devices:
         device_state = device.get_state()
         if device_state == 'device':
-            device_properties = device.get_properties()
-            emulator_name = device_properties.get('ro.kernel.qemu.avd_name', 'Unknown').replace('_', ' ')
             update_capabilities = {
-                'deviceName': emulator_name,
-                'platformVersion': device_properties.get('ro.build.version.release', 'Unknown')
+                'deviceName': device.get_properties().get('ro.kernel.qemu.avd_name', 'Unknown').replace('_', ' '),
+                'platformVersion': device.get_properties().get('ro.build.version.release', 'Unknown')
             }
             return update_capabilities
     return None
+
